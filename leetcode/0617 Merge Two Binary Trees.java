cc73709e2f350c87def825e69335a51872d0768a
wrdcz <574273435@qq.com>
2019-12-20 12:57:59 +0800

    Update 0617 Merge Two Binary Trees.java

--- leetcode/0617 Merge Two Binary Trees.java
+++ leetcode/0617 Merge Two Binary Trees.java
@@ -50,4 +50,34 @@ class Solution {
         }
         return t1;
     }
+
+    public TreeNode mergeTrees3(TreeNode t1, TreeNode t2) {
+        if (null == t1) return t2;
+        if (null == t2) return t1;
+        t1.val += t2.val;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(t1);
+        q.add(t2);
+        while (!q.isEmpty()) {
+            TreeNode a = q.poll();
+            TreeNode b = q.poll();
+            
+            if (null == a.left && null != b.left) {
+                a.left = b.left;
+            } else if (null != a.left && null != b.left) {
+                a.left.val += b.left.val;
+                q.add(a.left);
+                q.add(b.left);
+            }
+            
+            if (null == a.right && null != b.right) {
+                a.right = b.right;
+            } else if (null != a.right && null != b.right) {
+                a.right.val += b.right.val;
+                q.add(a.right);
+                q.add(b.right);
+            }
+        }
+        return t1;
+    }
 }
