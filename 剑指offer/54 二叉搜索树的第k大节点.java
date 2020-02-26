e50446a105b4703df38d01cff826f823b357f823
wrdcz <angczscn@gmail.com>
2020-02-27 00:36:05 +0800

    Update 54 二叉搜索树的第k大节点.java

--- 剑指offer/54 二叉搜索树的第k大节点.java
+++ 剑指offer/54 二叉搜索树的第k大节点.java
@@ -21,3 +21,30 @@ class Solution {
         add(root.right, arr);
     }
 }
+
+class Solution {
+    private int idx = 0;
+    private int res = 0;
+    
+    public int kthLargest(TreeNode root, int k) {
+        int sum = nodeSum(root);
+        find(root, sum - k + 1);
+        return res;
+    }
+    
+    private int nodeSum(TreeNode root) {
+        if (null == root) return 0;
+        int sum = 1;
+        sum += nodeSum(root.left);
+        sum += nodeSum(root.right);
+        return sum;
+    }
+    
+    private void find(TreeNode root, int aim) {
+        if (null == root) return;
+        find(root.left, aim);
+        ++idx;
+        if (idx == aim) res = root.val;
+        find(root.right, aim);
+    }
+}
