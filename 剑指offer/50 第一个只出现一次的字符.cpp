d2b7c2d105a5598c6aa60a0e0f45dc2164c2551f
wrdcz <574273435@qq.com>
2020-09-12 02:52:39 +0800

    Update 50 第一个只出现一次的字符.cpp

--- 剑指offer/50 第一个只出现一次的字符.cpp
+++ 剑指offer/50 第一个只出现一次的字符.cpp
@@ -1,3 +1,23 @@
+// solution1
+// time complexity O(n2)
+// space complexity O(1)
+class Solution {
+public:
+    char firstUniqChar(string s) {
+        if (s.length() == 1) return s[0];
+        for (int i = 0; i < s.length(); ++i) {
+            for (int j = 0; j < s.length(); ++j) {
+                if (i != j && s[i] == s[j]) break;
+                if (j == s.length() - 1) return s[i];
+            }
+        }
+        return ' ';
+    }
+};
+
+// solution1
+// time complexity O(n)
+// space complexity O(n)
 class Solution {
 public:
     char firstUniqChar(string s) {
