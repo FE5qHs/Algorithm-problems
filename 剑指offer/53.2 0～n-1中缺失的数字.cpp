182cce730b60ec0a3f80ded7cfff6be32bb2957b
wrdcz <574273435@qq.com>
2020-11-05 23:05:14 +0800

    Update 53.2 0～n-1中缺失的数字.cpp

--- 剑指offer/53.2 0～n-1中缺失的数字.cpp
+++ 剑指offer/53.2 0～n-1中缺失的数字.cpp
@@ -21,7 +21,7 @@ public:
     }
 };
 
-// solution1
+// solution2
 // time complexity O(n)
 // space complexity O(1)
 class Solution {
@@ -31,5 +31,28 @@ public:
             if (i != nums[i]) return i;
         }
         return nums.size();
+    }
+};
+
+// solution3
+// time complexity O(log(n))
+// space complexity O(1)
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int low = 0, high = nums.size();
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > mid) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        if (low == nums.size()) return low;
+        if (low == 0 && nums[low] != 0) return 0;
+        if (low > 0 && nums[low - 1] + 1 != nums[low]) return nums[low] - 1;
+        if (low < nums.size() - 1 && nums[low] + 1 != nums[low + 1]) return nums[low] + 1;
+        return 0;
     }
 };
