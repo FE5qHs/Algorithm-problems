a81d5bd2f8647919b128fb2de81dc972796767df
wrdcz <angczscn@gmail.com>
2020-12-30 01:59:56 +0800

    Update 58.2 左旋转字符串.cpp

--- 剑指offer/58.2 左旋转字符串.cpp
+++ 剑指offer/58.2 左旋转字符串.cpp
@@ -14,3 +14,25 @@ public:
         return res;
     }
 };
+
+// solution2
+// time complexity O(n)
+// space complexity O(1)
+class Solution {
+public:
+    string reverseLeftWords(string s, int n) {
+        swap(s, 0, s.length() - 1);
+        swap(s, 0, s.length() - 1 - n);
+        swap(s, s.length() - n, s.length() - 1);
+        return s;
+    }
+    
+    void swap(string& s, int left, int right) {
+        for (; left < right; ++left, --right) {
+            char temp = ' ';
+            temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+        }
+    }
+};
