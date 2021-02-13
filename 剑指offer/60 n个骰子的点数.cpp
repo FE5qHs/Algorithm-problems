e9e4fd3ecf974bf5fba32b61d04715e4ab8ea4fc
wrdcz <574273435@qq.com>
2021-02-13 17:16:26 +0800

    Update 60 n个骰子的点数.cpp

--- 剑指offer/60 n个骰子的点数.cpp
+++ 剑指offer/60 n个骰子的点数.cpp
@@ -19,3 +19,33 @@ public:
         }
     }
 };
+
+// solution2
+// time complexity O(n^2)
+// space complexity O(n^2)
+class Solution {
+public:
+    vector<double> dicesProbability(int n) {
+        int rows = n + 1, cols = 6 * n + 1;
+        int dp[rows][cols];
+        memset(dp, 0, sizeof(dp[0][0]) * rows * cols);
+        for (int i = 1; i <= 6; ++i) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= n; ++i) {
+            for (int j = i; j <= i * 6; ++j) {
+                int res = 0;
+                for (int k = 1; k <= 6 && k < j; ++k) {
+                    res += dp[i - 1][j - k];
+                }
+                dp[i][j] = res;
+            }
+        }
+        vector<double> res(5 * n + 1, 0);
+        double resBase = pow(6, n);
+        for (int i = 0; i < 5 * n + 1; ++i) {
+            res[i] = dp[n][n + i] / resBase;
+        }
+        return res;
+    }
+};
