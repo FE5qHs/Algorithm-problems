a6017de15b3bc3a8d7a10a32efdca6de284fa0d5
wrdcz <574273435@qq.com>
2021-04-17 18:39:55 +0800

    Update 62 圆圈中最后剩下的数字.cpp

--- 剑指offer/62 圆圈中最后剩下的数字.cpp
+++ 剑指offer/62 圆圈中最后剩下的数字.cpp
@@ -18,3 +18,29 @@ public:
         return arr[0];
     }
 };
+
+// solution2
+// time complexity O(nm)
+// space complexity O(n)
+class Solution {
+public:
+    int lastRemaining(int n, int m) {
+        list<int> l;
+        list<int>::iterator it;
+
+        for (int i = 0; i < n; ++i) l.push_back(i);
+        it = l.begin();
+
+        while (l.size() != 1) {
+            for (int i = 0; i < m - 1; ++i) {
+                ++it;
+                if (it == l.end()) it = l.begin();
+            }
+
+            it = l.erase(it);
+            if (it == l.end()) it = l.begin();
+        }
+
+        return *l.begin();
+    }
+};
