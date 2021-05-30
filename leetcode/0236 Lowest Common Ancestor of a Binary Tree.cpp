3cd91b25873f362581ba372a7f69f77ece7cd3cd
wrdcz <574273435@qq.com>
2021-05-30 22:32:56 +0800

    Update 0236 Lowest Common Ancestor of a Binary Tree.cpp

--- leetcode/0236 Lowest Common Ancestor of a Binary Tree.cpp
+++ leetcode/0236 Lowest Common Ancestor of a Binary Tree.cpp
@@ -1,3 +1,6 @@
+// solution1
+// time complexity O(n)
+// space complexity O(n)
 class Solution {
 public:
     TreeNode* result;
@@ -18,5 +21,66 @@ public:
 
         if (ret == 2 && result == nullptr) result = root;
         return ret;
+    }
+};
+
+// solution2
+// time complexity O(n)
+// space complexity O(n)
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr) return nullptr;
+
+        stack<TreeNode*> s;
+        s.push(root);
+
+        map<TreeNode*, TreeNode*> m;
+        m[root] = nullptr;
+
+        TreeNode* res1 = nullptr;
+        TreeNode* res2 = nullptr;
+        int cur = 0;
+        while (!s.empty()) {
+            TreeNode* tempNode = s.top();
+            s.pop();
+
+            if (tempNode == p || tempNode == q) {
+                if (res1 == nullptr) res1 = tempNode;
+                else res2 = tempNode;
+
+                if (++cur == 2) break;
+            }
+
+            if (tempNode->left != nullptr) {
+                s.push(tempNode->left);
+                m[tempNode->left] = tempNode;
+            }
+
+            if (tempNode->right != nullptr) {
+                s.push(tempNode->right);
+                m[tempNode->right] = tempNode;
+            }
+        }
+
+        set<TreeNode*> se;
+        TreeNode* res = nullptr;
+
+        while (res1 != nullptr) {
+            se.insert(res1);
+
+            res1 = m[res1];
+        }
+
+        while (res2 != nullptr) {
+            if (se.find(res2) != se.end()) {
+                res = res2;
+                break;
+            }
+            
+            res2 = m[res2];
+        }
+
+        return res;
     }
 };
